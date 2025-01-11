db44cea7b0063315ea2246916393d31657439e94
Erwin Liem <erwin.liem@stp.one>
2025-01-11 23:24:41 +0100

    script for traversing filepath

---
+++ traverse.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import optparse
+
+def traverse(path, limit_count, limit_min_file_size):
+    print("traversing the path: " + path)
+    print("limit_count: " + str(limit_count))
+    print("limit_min_file_size: " + str(limit_min_file_size))
+    _list = []
+    counter = 0
+    for _root, _dir, _files in os.walk(path):
+        for file in _files:
+            abs_path = os.path.join(_root, file)
+            stat = os.stat(abs_path)
+            # we only get file with this limit min size
+            if stat.st_size > limit_min_file_size:
+               _list.append((file, abs_path, stat.st_size))
+
+            # if we reach the limit_count, than stop it
+            if len(_list) == limit_count:
+                break
+        if len(_list) == limit_count:
+            break
+
+    return _list
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option('-p', '--path', help='specify the path to traverse')
+    parser.add_option('-c', '--limit_count', help='limited number of results')
+    parser.add_option('-f', '--limit_min_file_size', help='minimum limit of the file size in bytes')
+    opts, args = parser.parse_args()
+    if opts.path is None:
+        opts.path = "."
+    if opts.limit_count is None:
+        opts.limit_count = 20
+    if opts.limit_min_file_size is None:
+        opts.limit_min_file_size = 1000
+
+    files = traverse(opts.path, int(opts.limit_count), int(opts.limit_min_file_size))
+    if len(files) == 0:
+        print("no files found matching the criteria")
+    else:
+        print("this is what we found:")
+        for file in files:
+            print(file)

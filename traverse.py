72645809433c6481f00bb7a6a41a932669c50f64
Erwin Liem <erwin.liem@stp.one>
2025-01-11 23:38:49 +0100

    optimised setting default args values

--- traverse.py
+++ traverse.py
@@ -27,17 +27,20 @@ def traverse(path, limit_count, limit_min_file_size):
     return _list
 
 if __name__ == "__main__":
+    LIMIT_COUNT = 5
+    LIMIT_MIN_FILE_SIZE = 100000
+
     parser = optparse.OptionParser()
-    parser.add_option('-p', '--path', help='specify the path to traverse')
-    parser.add_option('-c', '--limit_count', help='limited number of results')
-    parser.add_option('-f', '--limit_min_file_size', help='minimum limit of the file size in bytes')
+    parser.add_option('-p', '--path', help='specify the path to traverse, default: current directory')
+    parser.add_option('-c', '--limit_count', help='limited number of results, default: {}'.format(LIMIT_COUNT))
+    parser.add_option('-f', '--limit_min_file_size', help='minimum limit of the file size in bytes, default: {}'.format(LIMIT_MIN_FILE_SIZE))
     opts, args = parser.parse_args()
     if opts.path is None:
         opts.path = "."
     if opts.limit_count is None:
-        opts.limit_count = 20
+        opts.limit_count = LIMIT_COUNT
     if opts.limit_min_file_size is None:
-        opts.limit_min_file_size = 1000
+        opts.limit_min_file_size = LIMIT_MIN_FILE_SIZE
 
     files = traverse(opts.path, int(opts.limit_count), int(opts.limit_min_file_size))
     if len(files) == 0:
